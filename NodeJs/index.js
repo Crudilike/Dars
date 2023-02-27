d5be9992187bd064568765a6f85030cb943cf18a
IslamDevFull <ehalim09@mail.ru>
2023-02-27 15:48:35 +0500

    Nodejs

---
+++ NodeJs/index.js
@@ -0,0 +1,11 @@
+var d = "Halim";
+
+const s = {
+    name: "Salom",
+}
+const f = [13]
+module.exports = {
+    Myname: d,
+    hello: s.name,
+    age: f
+}
---
+++ NodeJs/script.js
@@ -0,0 +1,2 @@
+var info = require(".//index");
+console.log(info.hello + " mening ismim " + info.Myname + " "  + "mening yoshim " + info.age + " da");
